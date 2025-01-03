b65a9fdcfdc9001891471789bc1bee859d3a72ea
Dmytro Osadchyi <tselina@gmail.com>
2025-01-03 22:27:38 +0100

    Task05

--- task05/jsrc/main/java/com/task05/Request.java
+++ task05/jsrc/main/java/com/task05/Request.java
@@ -3,9 +3,11 @@ package com.task05;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
+@NoArgsConstructor(force = true)
 @AllArgsConstructor
 @Data
 public class Request {
